64a02008f9e8580a064885065af6a4f7c0a4e496
Mateusz Śmiech <msmiech0@gmail.com>
2013-10-18 18:55:53 +0200

    Fixing Eclipse project import issues.

--- Algorithms/src/graph.h
+++ Algorithms/src/graph.h
@@ -1,129 +1,451 @@
 #include <iostream>
-#include <vector>
-#include <set>
 #include <queue>
+#include <vector>
+#include <fstream>
+#include <set>
 #include <stack>
 #include <algorithm>
-#include <fstream>
 
 using namespace std;
 
 typedef vector<int> VI;
+typedef vector<bool> VB;
+typedef pair<int, int> PII;
 
-#define VAR(a, b) typeof(a) b = (a)
-#define FOREACH(it, col) for(VAR((col).begin(), it); it != (col).end(); ++it)
+#define VAR(v,n) typeof(n) v=(n)
+#define FOREACH(i,c) for(VAR(i,(c).begin());i!=(c).end();++i)
 #define REP(x) for(int i = 0; i < x; i++)
-#define PB push_back
-#define ST first
-#define ND second
 #define SIZE(x) x.size()
+#define PB push_back
+#define ALL(x) x.begin(), x.end()
+#define MP make_pair
+#define ND second
+// (int) 2147483647 + (int) 2147483647 = -2
+const int INF = 1000000001;
 
-template<class Vertex, class Edge> struct Graph
+template<class Vertex, class Edge> struct Graph //Struktura generyczna reprezentująca graf.
 {
-	struct EdgeExtended : Edge
+	struct EdgeExtended : Edge //Rozszerza dodatkowe informacje o krawędzi o jej wierzchołek docelowy.
 	{
-		int v;
+		int destination; //Wierzchołek docelowy krawędzi.
 
-		EdgeExtended(int destination, Edge additional) : Edge(additional), v(destination)
+		//Konstruktor tworzy nową krawędź i ustala wierzchołek docelowy.
+		EdgeExtended(int dest, Edge additional = Edge()) : Edge(additional), destination(dest)
 		{
 
 		}
 	};
 
-	struct VertexExtended : Vertex, vector<EdgeExtended>
+	struct VertexExtended : Vertex, vector<EdgeExtended> //Rozszerza wektor w celu łatwiejszej iteracji.
 	{
 
 	};
 
-	vector<VertexExtended> g;
+	vector<VertexExtended> g; //Graf.
 
 	Graph(int n = 0) : g(n)
 	{
 
 	}
 
+	//Dodaje wierzchołek nieskierowany do grafu.
+	//Pole rev zawiera indeks krawędzi zwrotnej - krawędź nieskierowana reprezentowana jest przez dwie krawędzie skierowane.
+	void addUndirectedEdge(int u, int v, Edge eg = Edge())
+	{
+		EdgeExtended e(u, eg);
+		e.rev = SIZE(g[u]) + (u == v); //Szczególny przypadek - krawędź wychodzi z tego samego wierzchołka, do którego wchodzi.
+		g[v].PB(e);
+		e.destination = v;
+		e.rev = SIZE(g[v]) - 1; //Odejmujemy 1, ponieważ ta krawędź już została dodana, a pole zawiera indeks.
+		g[u].PB(e);
+	}
+
+	//Dodaje krawędź skierowaną do grafu.
 	void addDirectedEdge(int u, int v, Edge additional = Edge())
 	{
 		g[u].PB(EdgeExtended(v, additional));
 	}
 
-	void addUndirectedEdge(int u, int v, Edge additional = Edge())
-	{
-		EdgeExtended e(v, additional);
-		e.rev = SIZE(g[v]) + (u == v);
-		g[u].PB(e);
-		e.v = u;
-		e.rev = SIZE(g[u]) - 1;
-		g[v].PB(e);
-	}
-
+	//Wypisuje graf w rosnącej kolejności wierzchołków, następnie wierzchołki do których można dojść bezpośrednio z danego wierzchołka.
 	void write()
 	{
 		REP(SIZE(g))
 		{
-			cout << i << ": ";
+			cout << "Wierzchołek " << i << ": ";
 			FOREACH(it, g[i])
-				cout << it->v << " ";
+				cout << it->destination << " ";
 			cout << endl;
 		}
 	}
 
-	void BFS(int s)
+	//Przeszukiwanie grafu w głab od zadanego źródła.
+	void BFS(int source)
 	{
-		queue<int> Q;
 		FOREACH(it, g)
-			it->distance = it->ancestor = -1;
-		Q.push(s);
-		g[s].distance = 0;
-		while (!Q.empty())
+			it->ancestor = it->distance = -1; //Ustawiamy domyślne wartości przodka i odległości.
+		g[source].distance = 0; //d(u, u) = 0; - własność odległości (nie do końca, bo może istnieć krawędź o ujemnej wadze z wierzchołka do niego samego, ale to się prawie nie zdarza.
+		queue<int> q; //Kolejka z wierzchołkami do przetworzenia.
+		q.push(source); //Od czegoś trzeba zacząć.
+		while (!q.empty())
 		{
-			s = Q.front();
-			Q.pop();
-			FOREACH(it, g[s])
+			source = q.front(); //Aktualny wierzchołek.
+			q.pop();
+			FOREACH(it, g[source])
 			{
-				if (g[it->v].distance == -1)
+				if (g[it->destination].distance == -1) //Wierzchołek nie został jeszcze odwiedzony
 				{
-					g[it->v].distance = g[s].distance + 1;
-					g[it->v].ancestor = s;
-					Q.push(it->v);
+					g[it->destination].distance = g[source].distance + 1; //Aktualizujemy odległość.
+					g[it->destination].ancestor = source; //Ustawiamy rodzica w drzewie przeszukiwań wszerz.
+					q.push(it->destination); //I dodajemy wierzchołek do przetworzenia.
 				}
 			}
 		}
 	}
 
-	void DFS(int s = -1)
+	//Przeszukiwanie grafu w głąb z zadanego źródła.
+	void DFS(int source = -1)
 	{
 		FOREACH(it, g)
-			it->pre = it->post = it->ancestor = -1;
-		int time = 0;
-		if (s != -1)
-			time = DFSVisit(s, time);
+			it->pre = it->post = it->ancestor = -1; //Ustawiamy domyślne wartości czasu pre- i post-visit oraz przodka.
+		int time = 0; //Licznik czasu dla pre- i post-visit.
+		if (source != -1) //Źródło zostało dane.
+			time = DFSVisit(source, time);
 		REP(SIZE(g))
 		{
-			if (g[i].pre == -1)
-				time = DFSVisit(i, time);
+			if (g[i].pre == -1) //Wierzchołek nie został jeszcze odwiedzony.
+				time = DFSVisit(i, time); //Tworzymy nowe drzewo w lesie przeszukiwań w głąb.
 		}
 	}
 
-	int DFSVisit(int v, int time)
+	//Rekurencyjnie odwiedza graf w głąb.
+	int DFSVisit(int source, int time)
 	{
-		g[v].pre = time++;
-		FOREACH(it, g[v])
+		g[source].pre = time++; //Ustawia czas pre-visit.
+		FOREACH(it, g[source])
 		{
-			if (g[it->v].pre == -1)
+			if (g[it->destination].pre == -1) //Wierzchołek nie został jeszcze odwiedzony.
 			{
-				g[it->v].ancestor = v;
-				time = DFSVisit(it->v, time);
+				g[it->destination].ancestor = source; //Ustawiamy przodka w lesie przeszukiwań w głąb.
+				time = DFSVisit(it->destination, time); //Potrzebna jest rekurencyjna aktualizacja czasu.
 			}
 		}
-		g[v].post = time++;
+		g[source].post = time++; //Ustawia czas post-visit.
 		return time;
 	}
-};
 
-struct VertexBFS
-{
-	int distance, ancestor;
+	//Struktura definiująca niemalejący porządek wierzchołków względem ich odległości od źródła.
+	struct DijkstraComparator
+	{
+		bool operator() (const VertexExtended *a, const VertexExtended *b) const
+		{
+			if (a->distance == b->distance)
+				return a < b;
+			return (a->distance) < (b->distance);
+		}
+	};
+
+	//Algorytm Dijkstry znajdowania najkrótszych ścieżek z danego źródła w grafie ważonym.
+	//WAŻNE!! Wagi nie mogą być ujemne.
+	void Dijkstra(int s)
+	{
+		set<VertexExtended*, DijkstraComparator> q; //kolejka priorytetowa w postaci zbioru, ulepszeniem byłby kopiec Fibonacciego, ale jest to doś skomplikowana struktura danych
+		FOREACH(it, g)
+		{
+			it->distance = INF;
+			it->ancestor = -1;
+		}
+		g[s].distance = 0;
+		q.insert(&g[s]); //Dodajemy źródło do kolejki.
+		VertexExtended *v; //Wskaźnik do aktualnie przetwarzanego wierzchołka.
+		while (!q.empty())
+		{
+			v = *(q.begin()); //Odpowiednik top()
+			q.erase(q.begin()); //Odpowiednik pop()
+			FOREACH(it, *v) //Sprawdzamy wszystkie krawędzie.
+			{
+				if (g[it->destination].distance > v->distance + it->weight) //Dana krawędź może zmniejszyć wartość odległości dla wierzchołka docelowego.
+				{
+					//Tutaj wymagany jest myk z erase() i insert() - set nie obsługuje automatycznej obserwacji zmian elementów, musimy go usunąć i wstawić ponownie aby przetwarzał właściwą odległość.
+					q.erase(&g[it->destination]);
+					g[it->destination].distance = v->distance + it->weight;
+					g[it->destination].ancestor = v - &g[0]; //Odejmowanie wskaźników - odległość między elementami w tablicy (wektorze).
+					q.insert(&g[it->destination]);
+				}
+			}
+		}
+	}
+
+	//Algorytm Kosaraju - podwójny DFS. Stos jest symulowany za pomocą wektora.
+	//Obserwacja - graf transponowany ma silnie spójne składowe tożsame z oryginalnym.
+	//Θ(V+E) w przypadku list sąsiedztwa.
+	VI visited;
+	Graph<Vertex, Edge> *sccRes;
+
+	//W pierwszej fazie ustala porządek dla drugiej, później tworzy graf silnie spójnych składowych.
+	void sccDfs(int v, int nr, bool firstPhase)
+	{
+		g[v].t = 1; //Oznacza wierzchołek jako odwiedzony.
+		if (!firstPhase)
+			visited[v] = nr; //Numer silnie spójnej składowej.
+		FOREACH(it, g[v])
+		{
+			if (g[it->destination].t == -1) //Wierzchołek nie był odwiedzony, faza nie ma znaczenia.
+				sccDfs(it->destination, nr, firstPhase);
+			else if (!firstPhase && nr > visited[it->destination]) //Wierzchołek był odwiedzony, jesteśmy w drugiej fazie i krawędź prowadzi do zakończonej już (mniejszy numer) silnie spójnej składowej - trzeba dodać krawędź do grafu.
+				sccRes->addDirectedEdge(g[it->destination].t, visited[it->destination] = nr);
+		}
+		if (firstPhase)
+			visited.PB(v); //Dodaje wierzchołek do wektora 
+		else
+			g[v].t = nr;
+	}
+
+	Graph<Vertex, Edge> scc()
+	{
+		//gt - graf transponowany
+		//*tab[] wskazuje który graf (oryginalny czy transponowany) powinien być przetworzony w danej iteracji
+		//res - graf silnie spójnych składowych
+		Graph<Vertex, Edge> gt(SIZE(g)), res(SIZE(g)), *tab[] = {this, &gt};
+		visited.clear();
+		gt.visited.resize(SIZE(g), -1); //Wypełnia wektor SIZE(g) minus jedynkami.
+		gt.sccRes = &res; //Ustawia referencję do grafu silnie spójnych składowych w grafie transponowanym, na którym wykonywany będzie drugi przebieg algorytmu.
+		REP(SIZE(g))
+		{
+			FOREACH(it, g[i])
+				gt.addDirectedEdge(it->destination, i); //Tworzy graf transponowany.
+		}
+		REP(2)
+		{
+			FOREACH(it, tab[i]->g)
+				it->t = -1; //Pole t decyduje czy wierzchołek był odwiedzony, a po drugiej iteracji zawiera także numer silnie spójnej składowej.
+			int counter = 0, v; //Counter zawiera liczbę silnie spójnych składowych, v aktualnie przetwarzany wierzchołek.
+			for (int j = SIZE(g) - 1; j >= 0; j--) //W pierwszej iteracji to, że indeksy maleją nie ma znaczenia, ale jest wymagane podczas drugiej - tak działa algorytm.
+			{
+				if (tab[i]->g[v = i ? visited[j] : j].t == -1) //Podczas pierwszej iteracji odwiedzamy wierzchołek j, podczas drugiej - ten, który był odwiedzony jako j-ty (visited[j]).
+					tab[i]->sccDfs(v, counter++, 1 - i);
+			}
+			if (i)
+				res.g.resize(counter);
+		}
+		REP(SIZE(g))
+			g[i].t = gt.g[i].t; //Ustawia numery silnie spójnych składowych w grafie oryginalnym na bazie grafu transponowanego (patrz obserwacja na górze).
+		return res;
+	}
+
+	//Sortowanie topologiczne, zwraca wektor zawierający wierzchołki w posortowanej kolejności.
+	VI topoSortV()
+	{
+		VI sorted(SIZE(g));
+		topoSort();
+		REP(SIZE(g))
+			sorted[g[i].t] = i;
+		return sorted;
+	}
+
+	//Sortuje topologicznie, kolejność będzie zawarta w polu t.
+	void topoSort()
+	{
+		int index = SIZE(g) - 1;
+		FOREACH(it, g)
+		it->t = 0;
+		for (int i = SIZE(g) - 1; i >= 0; i--)
+			index = topoDfs(i, index);
+	}
+
+	//DFS do sortowania topologicznego.
+	int topoDfs(int v, int index)
+	{
+		if (!g[v].t)
+		{
+			g[v].t = 1;
+			FOREACH(it, g[v])
+				index = topoDfs(it->destination, index);
+			g[v].t = index--;
+		}
+		return index;
+	}
+
+	//Sprawdza, czy dany graf skierowany jest acykliczny.
+	bool isAcyclicDirected()
+	{
+		topoSort(); //Najpierw sortuje go topologicznie.
+		REP(SIZE(g))
+		{
+			FOREACH(it, g[i])
+			{
+				if (g[i].t >= g[it->destination].t) //Sprawdza czy wszystkie wierzchołki prowadzą do wierzchołków o mniejszym numerze.
+					return false;
+			}
+		}
+		return true;
+	}
+
+	VB vis;
+	bool acyclic;
+
+	//Sprawdza czy dany graf nieskierowany jest acykliczny.
+	bool isAcyclicUndirected()
+	{
+		acyclic = true;
+		vis.resize(SIZE(g), 0);
+		REP(SIZE(g))
+		{
+			if (!vis[i])
+				acyclicDfs(i, 0);
+		}
+		return acyclic;
+	}
+
+	void acyclicDfs(int v, Edge *e)
+	{
+		if (!vis[v]) //Wierzchołek nie był odwiedzony, jest ok.
+		{
+			vis[v] = true;
+			FOREACH(it, g[v])
+			{
+				if (&*it != e) //Nie chcemy się cofnąć krawędzią z której przyszliśmy.
+					acyclicDfs(it->destination, &g[it->destination][it->rev]);
+			}
+		}
+		else //Wierzchołek był odwiedzony, znaleziono cykl.
+			acyclic = false;
+	}
+
+	
+	bool EulerDirected(VI &path)
+	{
+		path.clear(); //Resetujemy wektor.
+		int edges = 1, v = -1, h;
+		stack<int> s;
+		VI degree(SIZE(g), 0); //Będzie zawierał stopnie wyjściowe wierzchołków.
+		REP(SIZE(g))
+		{
+			FOREACH(it, g[i])
+				degree[it->destination]++; //Które teraz ustawiamy.
+		}
+		REP(SIZE(g)) //Wyznaczamy wierzchołek początkowy.
+		{
+			if ((h = degree[i]) > SIZE(g[i])) //Jeśli ma większy stopień wyjściowy od wejściowego, zostaje nowym kandydatem na wierzchołek startowy.
+				v = i;
+			else if (v == -1 && h) //Jeśli nie jest izolowany i nie ma jeszcze wierzchołka startowego, to samo.
+				v = i;
+			edges += h;
+		}
+		if (v != -1) //Znaleziono wierzchołek początkowy, algorytm może przejść do następnego kroku.
+			s.push(v); 
+		while (!s.empty()) //Dopóki stos nie jest pusty, przeszukuje graf w głąb.
+		{
+			if (!degree[v = s.top()]) //Wszystkie krawędzie zostały przetworzone, cofamy się dodając krawędzie powrotne do ścieżki Eulera.
+			{
+				s.pop();
+				path.PB(v);
+			}
+			else
+				s.push(g[v][--degree[v]].destination);
+		}
+		reverse(ALL(path)); //Ścieżka Eulera wyznaczona jest w odwrotnym kierunku.
+		return edges == SIZE(path); //Prawda, jeśli wszyskie krawędzie należą do ścieżki.
+	}
+
+	//Algorytm działa tak samo jak dla grafów skierowanych, ale musi dodatkowo odznaczać wykorzystane krawędzie (występują niejako podwójnie - normalna i transponowana).
+	bool EulerUndirected(VI &path)
+	{
+		int v = -1;
+		path.clear();
+		VI degree(SIZE(g), 0), edges(SIZE(g) + 1, 0);
+		stack<int> s;
+		REP(SIZE(g))
+		{
+			edges[i + 1] = edges[i] + (degree[i] = SIZE(g[i]));
+			if (degree[i] || (v == -1 && degree[i]))
+				v = i;
+		}
+		vector<bool> used(edges[SIZE(g)], 0); //Wektor służący do odznaczania wykorzystanych krawędzi.
+		if (v != -1)
+			s.push(v);
+		while (!s.empty())
+		{
+			v = s.top();
+			while (degree[v] && used[edges[v] + degree[v] - 1])
+				--degree[v];
+			if (!degree[v])
+			{
+				s.pop();
+				path.PB(v);
+			}
+			else
+			{
+				int u = g[v][--degree[v]].destination;
+				used[edges[u] + g[v][degree[v]].rev] = 1;
+				s.push(v = u);
+			}
+		}
+		return 2 * (SIZE(path) - 1) == edges[SIZE(g)];
+	}
+
+	//Wyznacza minimalne drzewo rozpinające za pomocą algorytmu Prima.
+	//Pole span zawiera true jeśli krawędź należy do MST.
+	//Zwraca wagę drzewa.
+	int MinSpanTree()
+	{
+		int r = 0, d[SIZE(g)]; //Dla każdego wierzchołka nienależącego do MST przechowuje długość najkrótszej krawędzi łączącej go z dowolnym wierzchołkiem drzewa.
+		bool o[SIZE(g)]; //Odznacza wierzchołki dodane do drzewa.
+		REP(SIZE(g))
+		{
+			d[i] = INF;
+			o[i] = 0;
+		}
+		set<PII> s; //Kolejka priorytetowa wierzchołków osiągalnych z budowanego drzewa, w kolejności niemalejących wag krawędzi.
+		s.insert(MP(d[0] = 0, 0)); 
+		while (!s.empty())
+		{
+			int v = (s.begin())->ND; //Wybiera wierzchołek którego dodanie jest najtańsze.
+			s.erase(s.begin()); //Usuwa go z kolejki.
+			bool t = 0;  
+			o[v] = 1; //Zaznacza jako odwiedzony.
+			r += d[v];  //Zwiększa wagę drzewa.
+			FOREACH(it, g[v])
+			{
+				it->span = 0;
+				if (!t && o[it->destination] && it->weight == d[v]) //Krawędź, którą dodano do drzewa.
+					t = it->span = g[it->destination][it->rev].span = 1;
+				else if (!o[it->destination] && d[it->destination] > it->weight) //Aktualizujemy odległości pozostałych wierzchołków.
+				{
+					s.erase(MP(d[it->destination], it->destination));
+					s.insert(MP(d[it->destination] = it->weight, it->destination));
+				}
+			}
+		}
+		return r;
+	}
+
+	
+	bool BellmanFord(int v)
+	{
+		FOREACH(it, g)
+			it->distance = INF;
+		g[v].distance = 0;
+		g[v].ancestor = -1;
+		int change, cnt = 0;
+		do //Dopóki dokonana została poprawa, ale nie więcej niż SIZE(g) razy.
+		{
+			change = 0;
+			REP(SIZE(g))
+			{
+				FOREACH(it, g[i])
+				{
+					if (g[i].distance + it->weight < g[it->destination].distance) //Relaksuje krawędzie.
+					{
+						g[it->destination].distance = g[i].distance + it->weight;
+						g[it->destination].ancestor = i;
+						change = 1;
+					}
+				}
+			}
+		}
+		while (change && ++cnt < SIZE(g));
+		return cnt == SIZE(g); //true jeśli istnieje cykl o ujemnej wadze.
+	}
 };
 
 struct Empty
@@ -131,11 +453,51 @@ struct Empty
 
 };
 
-struct VertexDFS
+struct VertexBFS
 {
-	int pre, post, ancestor;
+	int distance, ancestor;
 };
 
+struct VertexDFS
+{
+	int ancestor, pre, post;
+};
+
+struct VertexDijkstra
+{
+	int distance, ancestor;
+};
+
+struct EdgeDijkstra
+{
+	int weight, rev;
+};
+
+struct EdgeUndirected
+{
+	int rev;
+};
+
+struct VertexScc
+{
+	int t;
+};
+
+struct MSTEdge
+{
+	int rev, weight;
+	bool span;
+};
+
+struct VertexBellmanFord
+{
+	int distance, ancestor;
+};
+
+struct EdgeBellmanFord
+{
+	int weight;
+};
 
 //Wierzchołek 0: 1
 //Wierzchołek 1: 2
@@ -201,7 +563,7 @@ void DFSTest()
 	REP(a)
 	cout << i << " - pre " << g.g[i].pre << " post " << g.g[i].post << " ancestor " << g.g[i].ancestor << endl;
 }
-/*
+
 //0 - distance 0 ancestor -1
 //1 - distance 10 ancestor 0
 //2 - distance 15 ancestor 0
@@ -394,9 +756,9 @@ void BellmanFordTest()
 	g.BellmanFord(s);
 	REP(n)
 		cout << i << " - distance " << g.g[i].distance << " ancestor " << g.g[i].ancestor << endl;
-}*/
+}
 
-int testAll()
+int main()
 {
 	graphTest();
 	cout << endl;
@@ -404,7 +766,7 @@ int testAll()
 	cout << endl;
 	DFSTest();
 	cout << endl;
-	/*DijkstraTest();
+	DijkstraTest();
 	cout << endl;
 	SccTest();
 	cout << endl;
@@ -416,5 +778,5 @@ int testAll()
 	cout << endl;
 	MSTTest();
 	cout << endl;
-	BellmanFordTest();*/
+	BellmanFordTest();
 }
